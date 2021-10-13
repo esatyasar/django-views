91a0aef7156248bb38bf174f468d2f2d21d24485
esatyasar <esatyasar91@gmail.com>
2021-10-13 22:32:24 +0300

    edited home.html

--- app/views.py
+++ app/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 def home (request):
-    return render(request, 'app/home.html')
+    context = {
+        'title': 'clarusway',
+        'dict1': {'django': 'best framework'},
+        'my_list': [1,2, 3, 4, 5]
+    }
+    return render(request, 'app/home.html', context)
